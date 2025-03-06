a3e6ebb479a4b08b3d1616f897d6b90e8f35c362
Tigana <tigana137@gmail.com>
2025-03-06 12:27:26 +0100

    integrate,update,template and taildwind

---
+++ Frontend/src/component/Home/index.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import Footer from "../Footer";
+import GoTopBtn from "../Button/GoTopBtn";
+import About from "../About/About";
+import Agents from "../Agents";
+import Blog from "../Blog";
+import DownloadApp from "../DownloadApp";
+import Features from "../Features";
+import Header from "../Header";
+import HomecHero from "../HomecHero";
+import LatestProperty from "../LatestProperty";
+import PropertyListing from "../PropertyListing";
+import Preloader from "../Loader";
+import FaqSection from "../Faq/FaqSection";
+
+function Home() {
+  const [isLoading, setisLoadingg] = useState(true);
+
+  useEffect(() => {
+    setisLoadingg(false);
+  }, []);
+
+  let component = undefined;
+  if (isLoading) {
+    component = <Preloader />;
+  } else {
+    component = (
+      <>
+        <h1 className="bg-blue-900 px-5   w-full border text-pink-500">hi from taildwind :</h1>
+        <Header />
+        <HomecHero />
+        <PropertyListing />
+        <About />
+        <LatestProperty />
+        <h1 className="bg-blue-900 px-5   w-full border text-pink-500">hi from taildwind :</h1>
+
+        <Features />
+        <Agents />
+        <FaqSection />
+        <DownloadApp />
+        <Blog />
+        <Footer />
+        <GoTopBtn />
+      </>
+    );
+  }
+  return component;
+}
+
+export default Home;
