a9a53b4aeaf01a2abcb275a1a04e3bc0c3963a39
Tigana <tigana137@gmail.com>
2025-03-19 13:05:14 +0100

    add zod to backend

--- Backend/src/Controllers/user.controller.ts
+++ Backend/src/Controllers/user.controller.ts
@@ -5,6 +5,7 @@ import User from '../Models/user.model';
 import errorMessages from "../utils/errorMessages";
 import generateToken from "../utils/generateJWT";
 import statusCode from "../utils/statusCode";
+import z from "zod";
 
 
 export const test = (req: Request, res: Response) => {
@@ -12,12 +13,44 @@ export const test = (req: Request, res: Response) => {
 };
 
 
+const registerBodySchema = z.object({
+    firstName: z.string()
+        .min(2, { message: "First name must be at least 2 characters long" })
+        .max(25, { message: "First name must be at most 25 characters long" }),
+
+    lastName: z.string()
+        .min(2, { message: "Last name must be at least 2 characters long" })
+        .max(25, { message: "Last name must be at most 25 characters long" }),
+
+    phoneNumber: z.string()
+        .min(5, { message: "Phone number must be at least 5 characters long" })
+        .max(17, { message: "Phone number must be at most 17 characters long" }),
+
+    email: z.string().email({ message: "Invalid email address" }),
+
+    password: z.string()
+        .min(6, { message: "Password must be at least 6 characters long" })
+        .max(25, { message: "Password must be at most 25 characters long" }),
+
+    confirmPassword: z.string()
+        .min(6, { message: "Confirm password must be at least 6 characters long" })
+        .max(25, { message: "Confirm password must be at most 25 characters long" }),
+
+}).refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+});
+
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
 
     const { firstName, lastName, password, email, role, phoneNumber } = req.body
 
-    if (!firstName || !lastName || !password || !email || !phoneNumber) return next(errorHandler(403, 'You are not allowed to update this user'));
+    // if (!firstName || !lastName || !password || !email || !phoneNumber) return next(errorHandler(403, 'You are not allowed to update this user'));
+
+    const validBody = registerBodySchema.safeParse(req.body)
+
+    if (!validBody.success) return next(errorHandler(404, "heyyyy", validBody.error.errors));
 
     const newUser = new User({
         firstName,
--- Backend/src/utils/error.ts
+++ Backend/src/utils/error.ts
@@ -1,7 +1,10 @@
+import { ZodIssue } from "zod";
 
-export const errorHandler = (statusCode: any, message: string) => {
+export const errorHandler = (statusCode: any, message: string, erros?: ZodIssue[]) => {
+    { erros && console.log(erros) }
     let error: any = {};
     error.statusCode = statusCode;
     error.message = message;
+    error.errors = erros;
     return error;
 };
