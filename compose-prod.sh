f29b0bb6b69feea0498da12f94d6050a2894393b
Tigana <tigana137@gmail.com>
2025-04-21 15:12:36 +0100

    refine prod dep

--- compose-prod.sh
+++ compose-prod.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Load production environment variables into the current shell
-set -o allexport
-source .env.production
-set +o allexport
+# set -o allexport
+# source .env.production
+# set +o allexport
 
 # Start services with base and prod override files
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d
