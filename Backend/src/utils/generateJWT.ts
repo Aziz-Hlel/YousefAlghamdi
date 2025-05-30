33d2e123a796b7506728603ca1fe6cf303db4c18
Tigana <tigana137@gmail.com>
2025-05-30 09:23:34 +0100

    change jwt life  time

--- Backend/src/utils/generateJWT.ts
+++ Backend/src/utils/generateJWT.ts
@@ -10,8 +10,8 @@ const REFRESH_SECRET = ENV.JWT_REFRESH_SECRET
 export const NODE_ENV = ENV.NODE_ENV;
 
 
-const accessTokenLifeSpan = "1d";
-const refreshTokenLifeSpan = "60d";
+const accessTokenLifeSpan = "5m";
+const refreshTokenLifeSpan = "30m";
 
 
 const generateAccessToken = (payload: { [key: string]: any }) => {
