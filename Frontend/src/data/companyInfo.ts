5eaffc34e3219544549e572976010c44d635081f
Tigana <tigana137@gmail.com>
2025-06-02 15:23:20 +0100

    add healthChecks

--- Frontend/src/data/companyInfo.ts
+++ Frontend/src/data/companyInfo.ts
@@ -1,5 +1,5 @@
 const companyInfo = {
-    email: "Info@ypg.ae",
+    email: "Info@ygp.ae",
     phone: "+971 525002822",
     address: "Ibis Hotel Rigga Business Center, 3rd floor Al Rigga , Muraqqabat , Dubai"
 }
